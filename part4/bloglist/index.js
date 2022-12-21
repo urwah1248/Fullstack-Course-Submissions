7c181c771f759b89e3b85a52a9532826ccf834a3
M Urwah Shahid <urwahpatel@gmail.com>
2022-12-22 01:34:26 +0500

    Finished Ex 4.1, Ex 4.2

---
+++ part4/bloglist/index.js
@@ -0,0 +1,10 @@
+const app = require('./app') // the actual Express application
+const http = require('http')
+const config = require('./utils/config')
+const { info, error } = require('./utils/logger')
+
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  info(`Server running on port ${config.PORT}`)
+})
