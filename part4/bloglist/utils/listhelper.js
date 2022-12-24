f26628185712fff7465c2ba59eb041c56748d46a
M Urwah Shahid <urwahpatel@gmail.com>
2022-12-25 00:55:15 +0500

    finished ex 4.5

--- part4/bloglist/utils/listhelper.js
+++ part4/bloglist/utils/listhelper.js
@@ -12,8 +12,15 @@ const totalLikes = (blogs) => {
     return likes
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.length === 0
+    ? {}
+    : blogs.reduce((maxLikes, blog) => blog.likes > maxLikes ? blog.likes : maxLikes, blogs[0].likes)
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
   
